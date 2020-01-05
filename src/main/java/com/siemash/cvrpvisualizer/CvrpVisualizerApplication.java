4342af3799ac8635438dff710473ce543ce4ba80
siemashko <k.siemaszko@student.mini.pw.edu.pl>
2020-01-05 18:12:43 +0100

    Initial commit.

---
+++ src/main/java/com/siemash/cvrpvisualizer/CvrpVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.siemash.cvrpvisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvrpVisualizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvrpVisualizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siemash/cvrpvisualizer/CvrpVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siemash.cvrpvisualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvrpVisualizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
